48963829546de62237f1afdb54a51bd7f6b70fbf
GuedesBF <bruno.guedes@hc.fm.usp.br>
2020-09-17 20:07:27 -0300

    Substituted Lilliefors normality test for Kolmogorov-smironv, which is inadequate

--- is.normal.R
+++ is.normal.R
@@ -1,22 +1,19 @@
 #given a vector x, a normality.test (default=="shapiro", alternative=="ks"), is.normal returns a logical value of TRUE if normality.test$p.value<=0.05 (non-normal),
 #FALSE if normality.test$pvalue>0.05 (normal) or NA if not numeric. A a density plot, and a qq plot will be ploted if "plots" argument is set to TRUE (true by default)
 
-is.normal<-function(x, normality.test="shapiro", plots=FALSE){
-        if (!normality.test %in% c("shapiro", "ks")){
-                stop("normality test must be either 'shapiro' or 'ks'")
+is.normal<-function(x, normality.test="shapiro", hist=FALSE){
+        if (!normality.test %in% c("shapiro", "lillie")){
+                stop("normality test must be either 'shapiro' or 'lillie'")
         }
-        library(stats)
-        library(ggplot2)
-        library(ggpubr)
+        library(nortest)
         if (is.numeric(x)){
-                if (plots==TRUE){
-                        dens<-plot(ggdensity(x))
-                        qq<-plot(ggqqplot(x))
+                if (hist==TRUE){
+                        hist(x)
                         }
                 if (normality.test=="shapiro"){
                 ifelse(shapiro.test(x)$p.value<=0.05, FALSE, TRUE)}
-                else if (normality.test=="ks"){
-                ifelse(ks.test(x, "pnorm", mean(x), sd(x))$p.value<=0.05, FALSE, TRUE)
+                else if (normality.test=="lillie"){
+                ifelse(lillie.test(x)$p.value<=0.05, FALSE, TRUE)
                 }
         } else {
                 NA
